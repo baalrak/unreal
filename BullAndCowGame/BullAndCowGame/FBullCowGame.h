f6bba8764f4d0673badc85200d3168efc2ec1302
Beau Johnson <baalrak@bigpond.com>
2017-06-25 12:03:31 +1000

    Commit of changes to source code before moving functions into classes

---
+++ BullAndCowGame/BullAndCowGame/FBullCowGame.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class FBullCowGame {
+public:
+
+
+private:
+};
--- BullAndCowGame/BullAndCowGame/main.cpp
+++ BullAndCowGame/BullAndCowGame/main.cpp
@@ -1,22 +1,62 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+
 using namespace std;
 
-int main() 
-{
-	// Constant expressions
-	constexpr int WORD_LENGTH = 5;
+void IntroMessage();
+	void PlayGame();
+	string GetGuess();
+	bool AskToPlayAgain();
+
+
+int main() {
+	// Call initial game output
+	do {
+		IntroMessage();
+		PlayGame();
+	} while (AskToPlayAgain());
+	return 0;
+}
+
+
+void IntroMessage() {
+	const int WORD_LENGTH = 5;
 	// Initial game output
 	cout << "Welcome to Bulls and Cows, a fun word game\n";
 	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I am thinking of?\n";
+	return;
+}
 
+
+void PlayGame() {
+	const int NUMBER_OF_GUESSES = 5;
+	// Get the guess from the player and repeat the guess back.
+	for (int i = 0; i < NUMBER_OF_GUESSES; i++) {
+		string guess = GetGuess();
+		cout << "You guessed: " << guess << "!\n";
+	}
+}
+
+
+string GetGuess() {
 	// Get a guess from the player
 	cout << "Enter your guess: ";
-	string Guess = "";
-	cin >> Guess;
+	string guess = "";
+	getline(cin, guess);
+	return guess;
+}
 
-	// Repeat the guess back to the player
-	cout << "You guessed: " << Guess << "!\n";
 
-	return 0;
+bool AskToPlayAgain() {
+	cout << "Do you want to play again? [Y/N]";
+	string playAgain = "";
+	getline(cin, playAgain);
+	transform(playAgain.begin(), playAgain.end(), playAgain.begin(), ::toupper);
+	if (playAgain.compare("Y") == 0) {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
